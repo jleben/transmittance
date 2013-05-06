9fd1ad7faa08d6052d81d238133add326993420d
Jakob Leben <jakob.leben@gmail.com>
2013-05-06 21:33:28 +0200

    minibee: add run script

---
+++ minibee/run.sh
@@ -0,0 +1,6 @@
+/home/jakob/projects/transmittance_u3/local/bin/python \
+/home/jakob/projects/transmittance_u3/local/lib/python2.7/site-packages/pydon/minihiveosc.py \
+-c /home/jakob/projects/transmittance_u3/minibee/config.xml \
+-a True \
+$1
+
